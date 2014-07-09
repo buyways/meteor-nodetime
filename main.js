3decab9f00211d4a1cd468b009dfab93b30213e8
Michel Hiemstra <mhiemstra@php.net>
2014-07-09 15:29:10 +0200

    custom code

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
+/**
+ * development accountkey by default
+ */
+var accountKey = "bd10b785c143c25cf73f2a0418a24b0c249bf716";
 
-Nodetime = Npm.require('nodetime');
+if(process.env.MRT_ENV == 'production') {
+    accountKey = "678073940e947d06009698cc26073f7b40385167";
+}
+
+Nodetime = Npm.require('nodetime').profile({
+    accountKey: accountKey,
+    appName: 'Arriva Intranet'
+});
+
+console.log('Nodetime loaded', accountKey, (process.env.MRT_ENV ? process.env.MRT_ENV : 'No environment'));
