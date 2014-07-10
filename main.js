9430a91d1a9ddc2ef48adf4d60f4e7a440ddc104
Michel Hiemstra <mhiemstra@php.net>
2014-07-10 11:32:46 +0200

    custom

--- main.js
+++ main.js
@@ -1,15 +1,9 @@
-/**
- * development accountkey by default
- */
-var accountKey = "bd10b785c143c25cf73f2a0418a24b0c249bf716";
 
-if(process.env.MRT_ENV == 'production') {
-    accountKey = "678073940e947d06009698cc26073f7b40385167";
+if (typeof Meteor.config.nodetimeApiKey !== "undefined") {
+    Nodetime = Npm.require('nodetime').profile({
+        accountKey: Meteor.config.nodetimeApiKey,
+        appName: 'Nodetime'
+    });
+
+    console.log('Nodetime loaded', Meteor.config.nodetimeApiKey, (process.env.MRT_ENV ? process.env.MRT_ENV : 'No environment'));
 }
-
-Nodetime = Npm.require('nodetime').profile({
-    accountKey: accountKey,
-    appName: 'Intranet'
-});
-
-console.log('Nodetime loaded', accountKey, (process.env.MRT_ENV ? process.env.MRT_ENV : 'No environment'));
